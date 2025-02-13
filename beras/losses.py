35ffcbe144a47b6b4c57d3860a55e3b141832814
Adeethyia Shankar <adeethyia_shankar@brown.edu>
2025-02-12 21:42:37 -0500

    Finished(ish) losses.py

--- beras/losses.py
+++ beras/losses.py
@@ -19,14 +19,15 @@ class MeanSquaredError(Loss):
         return Tensor(np.mean((y_pred - y_true)**2, keepdims=True))
 
     def get_input_gradients(self) -> list[Tensor]:
-        # TODO: ensure I am accessing y_pred and y_true correctly
+        # TODO: ensure I am accessing y_pred [0] and y_true [1] correctly
         return [Tensor(2 * np.mean(input[0] - input[1])) for input in self.inputs]
 
 class CategoricalCrossEntropy(Loss):
     def forward(self, y_pred, y_true):
         """Categorical cross entropy forward pass!"""
-        return NotImplementedError
+        return Tensor(-np.sum(np.dot(y_true, np.log(y_pred))))
 
     def get_input_gradients(self):
         """Categorical cross entropy input gradient method!"""
-        return NotImplementedError
+        # TODO: ensure y_pred [0] and y_true [1] are what I think they are
+        return [Tensor(-np.sum(np.where(input[0] == 0, 0, np.reciprocal(input[1])))) for input in self.inputs]
